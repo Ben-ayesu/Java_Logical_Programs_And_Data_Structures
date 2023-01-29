cf07f4cbdbb56f514d1d0eeeb669258b74a9b727
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-01-28 18:18:35 -0800

    SumsOfADigit Solved and Starting Assignment 1

---
+++ src/Section2/Assignment1.java
@@ -0,0 +1,7 @@
+package Section2;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Section2/SumsOfADigit.java
@@ -0,0 +1,20 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class SumsOfADigit {
+    // Sum of all the digits
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int digit, num = scanner.nextInt();
+        int sums = 0;
+
+        while (num != 0) {
+            digit = num % 10;
+            sums += digit;
+            num = num / 10;
+        }
+        System.out.println("The sum is: " + sums);
+    }
+}
