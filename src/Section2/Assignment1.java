d6c796507f55b59196c9f5dd2fd63f22d0187054
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-01-28 19:28:38 -0800

    SumsOfADigit Solved and Starting Assignment 1

--- src/Section2/Assignment1.java
+++ src/Section2/Assignment1.java
@@ -1,7 +1,25 @@
 package Section2;
 
+import java.util.Scanner;
+
 public class Assignment1 {
     public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int digit, num = scanner.nextInt();
+        int evenSums = 0;
+        int oddSums = 0;
 
+        while (num != 0) {
+            digit = num % 10;
+            if (digit % 2 == 0){
+                evenSums += digit;
+            } else {
+                oddSums += digit;
+            }
+            num = num / 10;
+        }
+        System.out.println("The sum of evens are : " + evenSums);
+        System.out.println("The sum of odds are : " + oddSums);
     }
 }
