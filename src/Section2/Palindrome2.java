f975f4cb6e229bc0e39cb0c96bf906550681e110
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-01 08:42:50 -0800

    Palindrome2 completed

---
+++ src/Section2/Palindrome2.java
@@ -0,0 +1,15 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class Palindrome2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.next();
+        StringBuffer sb = new StringBuffer(number);
+        String reversedNumber = sb.reverse().toString();
+
+        System.out.println(number.equals(reversedNumber) ? number + " is a Palindrone" : number + " is not a Palindrone");
+    }
+}
