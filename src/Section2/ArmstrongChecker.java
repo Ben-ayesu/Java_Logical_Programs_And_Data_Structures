2dc76ef84844a0b39b04a9204773ddc3b62e9095
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-05 00:01:23 -0800

    ArmstrongChecker completed

---
+++ src/Section2/ArmstrongChecker.java
@@ -0,0 +1,20 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class ArmstrongChecker {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int digit, num = scanner.nextInt();
+        int temp = num, sum = 0;
+
+        while (num != 0) {
+            digit = num % 10;
+            sum += digit * digit * digit;
+            num = num / 10;
+        }
+        System.out.println(temp == sum? "Armstrong Number": "Not an Armstrong Number");
+        scanner.close();
+    }
+}
