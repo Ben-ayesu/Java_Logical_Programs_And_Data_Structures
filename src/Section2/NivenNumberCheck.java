8384ac83147d36402b16882b7f1badad645ab8a0
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-04 22:57:55 -0800

    NivenNumberCheck completed

---
+++ src/Section2/NivenNumberCheck.java
@@ -0,0 +1,26 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class NivenNumberCheck {
+    // A Niven number is a positive integer that is divisible by the sum of its digits.
+    // For example, the number 18 is a Niven number because it is divisible by 1 + 8 = 9.
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println(num%getSums(num)==0? "Niven Number": "Not a Niven Number");
+    }
+
+    private static int getSums(int num) {
+        int digit;
+        int sums = 0;
+        while (num != 0) {
+            digit = num % 10;
+            sums += digit;
+            num = num / 10;
+        }
+        return sums;
+    }
+}
