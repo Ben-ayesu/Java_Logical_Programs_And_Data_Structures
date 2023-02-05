fe44f03f69e9c3e4fe6ae91b9973a0901f79dea0
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-04 21:09:51 -0800

    IntergertoBinary completed

---
+++ src/Section2/IntergertoBinary.java
@@ -0,0 +1,24 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class IntergertoBinary {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int[] binaryNum = new int[10];
+
+        int i = 0;
+
+        while (num != 0) {
+            binaryNum[i] = num % 2;
+            num = num / 2;
+            i++;
+        }
+
+        for (int j = i-1; j >= 0; j--) {
+            System.out.println(binaryNum[j]);
+        }
+    }
+}
