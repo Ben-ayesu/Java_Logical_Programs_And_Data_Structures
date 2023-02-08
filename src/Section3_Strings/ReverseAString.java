fb4a47d288d7a081b48746fc02dfab37156ac8a8
Benjamin Ayesu-Attah <bayesuattah@bestbuycanada.ca>
2023-02-08 15:59:09 -0800

    Completed ReverseAString

--- src/Section3_Strings/ReverseAString.java
+++ src/Section3_Strings/ReverseAString.java
@@ -1,5 +1,17 @@
 package Section3_Strings;
 
-public class ReverseAString {
+import java.util.Scanner;
 
+public class ReverseAString {
+    public static void main(String[] args) {
+        System.out.println("Enter a string: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        StringBuilder output = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            output.append(input.charAt(i));
+        }
+        System.out.println(output);
+    }
 }
