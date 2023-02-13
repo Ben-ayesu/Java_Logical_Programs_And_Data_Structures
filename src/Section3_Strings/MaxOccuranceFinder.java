a70323cddcc50fcdf2c9dd209204c1aeb43d5040
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-12 22:53:25 -0800

    Max occurence finder completed

---
+++ src/Section3_Strings/MaxOccuranceFinder.java
@@ -0,0 +1,26 @@
+package Section3_Strings;
+
+import java.util.Scanner;
+
+public class MaxOccuranceFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = scanner.nextLine();
+        int arr[] = new int [256];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[(int)s.charAt(i)]++;
+        }
+
+        int max = 0;
+
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] > arr[max]) {
+               max = j;
+            }
+        }
+
+        System.out.println("Max occuring character is: " + (char)max);
+    }
+}
